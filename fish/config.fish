bdee7d333c4db6878f6aedb0dfb04977ef5caf0f
Kim Desrosiers <kimdesro@gmail.com>
2016-03-15 21:54:13 -0400

    Manage go environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,9 @@ set -x EDITOR vim
 # Remove the greeting message
 set fish_greeting
 
+# Paths
+test -d /usr/local/go/bin ; and set PATH /usr/local/go/bin $PATH
+
 # Some git alias
 alias gst "git status"
 alias gdi "git diff"
@@ -16,3 +19,12 @@ alias gcm "git checkout master"
 # Set shellder config
 set -g theme_display_user yes
 set -g theme_hostname always
+
+# Set GOPATH
+if type go >/dev/null
+  set -x GOPATH ~/local/go
+  mkdir -p $GOPATH
+  if test -d $GOPATH/bin
+    set PATH $PATH $GOPATH/bin
+  end
+end
