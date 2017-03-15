4e6a9632a16d2ce42ad333fb1cab3ff058e880c9
Kim Desrosiers <kimdesro@gmail.com>
2017-03-15 18:52:37 -0400

    Put display name in variable to simplify the i3 config

--- i3/polybar-launch.sh
+++ i3/polybar-launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-MONITOR=eDP1 polybar mybar -c ~/.config/i3/polybar.conf &
+MONITOR=eDP-1 polybar mybar -c ~/.config/i3/polybar.conf &
 MONITOR=HDMI-2 polybar mybar -c ~/.config/i3/polybar.conf &
 
 echo "Bars launched..."
