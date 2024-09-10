a9823c20c8ca67f980e013de174a9f58bb9184dc
Kim Desrosiers <kimdesro@gmail.com>
2024-09-10 10:06:12 -0400

    Update nvim highlight config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "LspReferenceText" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "LspReferenceWrite" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "LspReferenceRead" })
+    vim.api.nvim_set_hl(0, "LspReferenceRead", { underline = true })
+    vim.api.nvim_set_hl(0, "LspReferenceWrite", { underline = true })
+    vim.api.nvim_set_hl(0, "LspReferenceText", { underline = true })
+  end,
+})
