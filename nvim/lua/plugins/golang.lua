3df3066db3c7f62726928b9a8a793319edbe9803
Kim Desrosiers <kimdesro@gmail.com>
2025-03-16 15:06:00 -0400

    Improve golang setup

---
+++ nvim/lua/plugins/golang.lua
@@ -0,0 +1,27 @@
+vim.api.nvim_create_user_command("Golines", function()
+  return require("conform").format({
+    -- async means discarded if the file is modified before the formatting is done
+    async = true,
+    lsp_format = "never",
+    bufnr = 0,
+    formatters = { "golines" },
+  })
+end, {})
+
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = { "golangci-lint", "golines" },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        go = { "golangcilint" },
+      },
+    },
+  },
+}
